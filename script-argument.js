1e2c1ea6adb66f7edb86df6e3064dfe27d649ea6
aleksandr-wegele <aleksandr.wegele@gmail.com>
2022-08-02 20:24:04 +0300

    feat: add arguments

---
+++ script-argument.js
@@ -0,0 +1,7 @@
+//параметры или аргументы
+function printHello(firstName){
+  alert ('Добрый день, ' + firstName + '!')
+}
+
+printHello('Иван')
+printHello('Медвед')
---
+++ script-function.js
@@ -0,0 +1,13 @@
+//function dicloration (объявление функции)
+function printHello (){
+  alert('Привет!')
+}
+
+printHello()
+
+//function expression
+let showHello = function (){
+  alert('Privet Medved!')
+}
+
+showHello()
